03e076606bea1d157a8690e62600139cc0a242f2
Tibor Korsos <tiborkorsos@gmail.com>
2023-06-17 15:39:34 +0200

    feltoltes-230617

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// popover
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+// tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
